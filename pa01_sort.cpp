d53a837e51b972df4318f50e2133da02dc42818f
kjmin017 <kjmin017@gmail.com>
2022-03-17 20:03:36 +0900

    heapsort update

--- pa01_sort.cpp
+++ pa01_sort.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <iostream>
+#include <cstdio>
 #include <algorithm>
 using namespace std;
-
+int quickcount = 0;
+int heapcount = 0;
 void selectsort(int list[], int n, int start, int pass) {
     int least = start;
     int i;
@@ -16,21 +18,85 @@ void selectsort(int list[], int n, int start, int pass) {
         }
         if(list[start] != min)
             swap(list[start], list[least]);
-        selectsort(list, n, start + 1, pass);
-    }
-    else {
         for (int i = 0; i < n; i++) {
-            if ((i + 1) % pass == 0) {
+            if ((start+1) == pass) {
                 cout << list[i] << endl;
             }
         }
+        selectsort(list, n, start + 1, pass);
     }
 }
-void heapsort() {
-
+void updateheap(int list[], int n, int pass) {
+    if (heapcount == pass) {
+        for (int i = 0; i <=n ; i++) {
+            cout << list[i] << endl;
+        }
+    }
+    swap(list[0], list[n]);
+    int root = 0;
+    int child = 1;
+    do {
+        child = 2 * root + 1;
+        if (list[child] > list[child + 1] && child < n - 1) {
+            child++;
+        }
+        if (list[root] > list[child] && child < n) {
+            swap(list[root], list[child]);
+        }
+        root = child;
+    } while (child < n);
+    heapcount++;
+    updateheap(list, n - 1, pass);
 }
-void mergesort() {
+void heapsort(int list[], int n, int pass) {
+    for (int i = 1; i < n; i++) {
+        int child = i;
+        do {
+            int root = (child - 1) / 2;
+            if (list[root] > list[child]) {
+                swap(list[root], list[child]);
+            }
+            child = root;
+        } while (child != 0);
+    }
+    
+    updateheap(list, n - 1, pass);
+}
 
+int partition(int list[], int left, int right) {
+    int pivot;
+    int low, high;
+
+    low = left;
+    high = right;
+    pivot = list[left];
+
+    do {
+        do {
+            low++;
+        } while (low <= right && list[low] < pivot);
+        do {
+            high--;
+        } while (high >= left && list[high] > pivot);
+        if (low < high) {
+            swap(list[low], list[high]);
+        }
+    } while (low < high);
+    swap(list[left], list[high]);
+    return high;
+}
+void quicksort(int list[], int left, int right, int pass, int n) {
+    if (left < right) {
+        int q = partition(list, left, right);
+        quickcount++;
+        if (quickcount == pass) {
+            for (int i = 0; i < n; i++) {
+                cout << list[i] << endl;
+            }
+        }
+        quicksort(list, left, q - 1, pass, n);
+        quicksort(list, q + 1, right, pass, n);
+    }
 }
 int main(void) {
     int sortkind, pass;
@@ -47,8 +113,12 @@ int main(void) {
             selectsort(arr, N, 0, pass);
             break;
         case 2:
+            heapsort(arr, N, pass);
+            heapcount = 0;
             break;
         case 3:
+            quicksort(arr, 0, N, pass, N);
+            quickcount = 0;
             break;
         default:
             break;
